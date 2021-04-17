7cf59707c4b87a991e2d598a4df221c03cf87e4c
rock-zion <rock_zion@outlook.com>
2021-04-17 19:58:44 +0100

    finished up nav: added links and final styling

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -1,15 +1,41 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { NavigationStyle } from "./style.module";
 import logo from "../../assets/icons/logo.svg";
+import droid from "../../assets/icons/droid.svg";
 import menuIcon from "../../assets/icons/menu-icon.svg";
 import closeIcon from "../../assets/icons/close-icon.svg";
+import { Link } from "react-router-dom";
+import { useLocation } from "react-router-dom";
+
+const links = [
+  "films",
+  "people",
+  "planets",
+  "species",
+  "starships",
+  "vehicles",
+];
 
 const Navigation = () => {
   const [drawer, toggleDrawer] = useState(false);
+  const location = useLocation();
+  console.log(location);
   return (
     <NavigationStyle>
       <img className={"logo"} src={logo} alt="starwars logo" />
-      <div className={`links webMenu`}></div>
+      <div className={`links webMenu`}>
+        {links.map((link) => (
+          <Link
+            className={`${
+              location.pathname.includes(link) ? "webMenuLinkActive" : ""
+            } webMenuLink`}
+            key={link}
+            to={`/${link}`}
+          >
+            {link.toUpperCase()}
+          </Link>
+        ))}
+      </div>
       <img
         onClick={() => toggleDrawer(!drawer)}
         className={`mobile menu`}
@@ -28,6 +54,19 @@ const Navigation = () => {
           src={closeIcon}
           alt=""
         />
+        <div className={`mobileLinkContainer`}>
+          <img className={`droid`} src={droid} alt="star wars droid" />
+          {links.map((link) => (
+            <Link
+              onClick={() => toggleDrawer(!drawer)}
+              className={`mobileLink`}
+              key={link}
+              to={`/${link}`}
+            >
+              {link.toUpperCase()}
+            </Link>
+          ))}
+        </div>
       </div>
     </NavigationStyle>
   );
