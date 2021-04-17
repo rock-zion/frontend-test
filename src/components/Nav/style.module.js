0cf0cc821cb9c340afe931af52b746cf94250a38
rock-zion <rock_zion@outlook.com>
2021-04-17 19:39:31 +0100

    finished up nav: added links and final styling

--- src/components/Nav/style.module.js
+++ src/components/Nav/style.module.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import backgroundShimmer from "../../assets/icons/bg-shimmer.gif";
 
 export const NavigationStyle = styled.header`
   height: 70px;
@@ -17,6 +18,33 @@ export const NavigationStyle = styled.header`
     display: none;
   }
 
+  .mobileMenu {
+    display: none;
+  }
+
+  .webMenuLink {
+    color: #ffffff;
+    margin-right: 8px;
+    text-decoration: none;
+    padding: 8px 0;
+    margin-right: 32px;
+    transition: all 0.2s;
+  }
+
+  .webMenuLink:last-child {
+    margin-right: 0;
+  }
+
+  .webMenuLinkActive,
+  .webMenuLink:hover {
+    background-image: url(${backgroundShimmer});
+    background-size: cover;
+    margin-right: 32px;
+    text-decoration: none;
+    transform: scale(1.1);
+    border-bottom: 2px solid red;
+  }
+
   .mobileMenuCloseIcon {
     display: none;
   }
@@ -24,12 +52,38 @@ export const NavigationStyle = styled.header`
   @media only screen and (max-width: 600px) {
     padding: 0 32px;
 
+    .webMenu {
+      display: none;
+    }
+
+    .droid {
+      position: absolute;
+      height: 300px;
+      bottom: 0;
+      right: 0;
+    }
+
+    .mobileLinkContainer {
+      margin: 100px 0 0 32px;
+      display: flex;
+      flex-direction: column;
+    }
+
+    .mobileLink {
+      margin-bottom: 32px;
+      font-weight: 700;
+      font-size: 24px;
+      color: #ffffff;
+      text-decoration: none;
+    }
+
     .menu {
       height: 32px;
       display: block;
     }
 
     .mobileMenuCloseIcon {
+      display: block;
       position: absolute;
       height: 32px;
       top: 19px;
@@ -37,6 +91,7 @@ export const NavigationStyle = styled.header`
     }
 
     .mobileMenu {
+      display: block;
       position: fixed;
       top: 0;
       right: 0;
