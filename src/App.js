31986bb50eb90b5c4163c3de1dc7e375ba24269c
rock-zion <rock_zion@outlook.com>
2021-04-18 12:21:47 +0100

    added context provider

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import {
 } from "react-router-dom";
 import * as Pages from "./pageloader";
 import { QueryClient, QueryClientProvider } from "react-query";
+import FavouritesContextProvider from "./contexts/FavouritesContext";
 import Navigation from "./components/Nav";
 import { Toaster } from "react-hot-toast";
 
@@ -14,23 +15,25 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <div className="App">
-      <QueryClientProvider client={queryClient}>
-        <Toaster />
-        <Router>
-          <Navigation />
-          <Switch>
-            <Route path="/" exact>
-              <Redirect to="/films" />
-            </Route>
-            <Route path="/films" exact component={Pages.HomePage} />
-            <Route path="/people" exact component={Pages.Characters} />
-            <Route path="/planets" exact component={Pages.Planets} />
-            <Route path="/species" exact component={Pages.Species} />
-            <Route path="/starships" exact component={Pages.Starships} />
-            <Route path="/vehicles" exact component={Pages.Vehicles} />
-          </Switch>
-        </Router>
-      </QueryClientProvider>
+      <FavouritesContextProvider>
+        <QueryClientProvider client={queryClient}>
+          <Toaster />
+          <Router>
+            <Navigation />
+            <Switch>
+              <Route path="/" exact>
+                <Redirect to="/films" />
+              </Route>
+              <Route path="/films" exact component={Pages.HomePage} />
+              <Route path="/people" exact component={Pages.Characters} />
+              <Route path="/planets" exact component={Pages.Planets} />
+              <Route path="/species" exact component={Pages.Species} />
+              <Route path="/starships" exact component={Pages.Starships} />
+              <Route path="/vehicles" exact component={Pages.Vehicles} />
+            </Switch>
+          </Router>
+        </QueryClientProvider>
+      </FavouritesContextProvider>
     </div>
   );
 }
