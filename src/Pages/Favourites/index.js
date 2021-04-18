3d62e3c11f73722cb6d0e5223da22ef7ba4eedee
rock-zion <rock_zion@outlook.com>
2021-04-18 13:25:14 +0100

    added Favourites page

---
+++ src/Pages/Favourites/index.js
@@ -0,0 +1,28 @@
+import React, { useContext } from "react";
+import { PagesLayout } from "../../components/Layout";
+import bg from "../../assets/images/lightsaber.jpeg";
+import FavouriteCard from "../../components/FavouriteCard";
+import { FavouritesContext } from "../../contexts/FavouritesContext";
+
+const HomePage = () => {
+  const { userFavourites } = useContext(FavouritesContext);
+  return (
+    <PagesLayout bannerText={"Favourites"} bgImg={bg}>
+      <div className="movieContainerParent">
+        {userFavourites &&
+          userFavourites.map((data) => (
+            <FavouriteCard
+              key={data.name}
+              resource={data.resource}
+              title={data.name}
+              seeMore={data.url}
+            />
+          ))}
+      </div>
+    </PagesLayout>
+  );
+};
+
+export default HomePage;
+
+HomePage.defaultProps = {};
