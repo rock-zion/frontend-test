68c975376589d4a509855450ee6fa61b3fc45c0d
rock-zion <rock_zion@outlook.com>
2021-04-18 12:28:09 +0100

    button layouts now reflects favourites

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,5 +1,10 @@
-import React from "react";
-import { PagesLayoutStyles } from "./style.module";
+
+import React, { useContext } from "react";
+import { PagesLayoutStyles, ButtonFavouriteLayoutStyle } from "./style.module";
+import { useHistory } from "react-router-dom";
+import unfavourited from "../../assets/icons/heart-line.svg";
+import favourited from "../../assets/icons/heart-fill.svg";
+import { FavouritesContext } from "../../contexts/FavouritesContext";
 
 export const PagesLayout = ({ children, bgImg, bannerText }) => {
   return (
@@ -12,3 +17,32 @@ export const PagesLayout = ({ children, bgImg, bannerText }) => {
     </PagesLayoutStyles>
   );
 };
+
+export const ButtonFavouriteLayout = ({ resource, url, name }) => {
+  const history = useHistory();
+  const { addToArray, userFavourites, removeFromArray } = useContext(
+    FavouritesContext
+  );
+  const handleLearnMore = () => {
+    history.push({ pathname: `${resource}/${name}`, url: { url } });
+  };
+  return (
+    <ButtonFavouriteLayoutStyle>
+      <button onClick={() => handleLearnMore()}>Learn More</button>
+      {userFavourites.find((fav) => fav.name === name) ? (
+        <img
+          onClick={() => removeFromArray(resource, url, name)}
+          alt=""
+          src={favourited}
+        />
+      ) : (
+        <img
+          onClick={() => addToArray(resource, url, name)}
+          alt=""
+          src={unfavourited}
+        />
+      )}
+    </ButtonFavouriteLayoutStyle>
+  );
+};
+
